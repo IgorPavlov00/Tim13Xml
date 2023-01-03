ebaaf37a0a2275e7092675f7e485eb4ced137431
IgorPavlov123 <igorpavlov106@gmail.com>
2023-01-03 16:21:31 +0100

    dodato upisivanje i ucitavanje a1.xml dokumenta u bazu

--- XmlProjekat/XmlProjekat/src/main/java/Tim13/XmlProjekat/Kontroleri/IndexController.java
+++ XmlProjekat/XmlProjekat/src/main/java/Tim13/XmlProjekat/Kontroleri/IndexController.java
@@ -1,13 +1,13 @@
 package Tim13.XmlProjekat.Kontroleri;
 
+import Tim13.XmlProjekat.util.ConnectionProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -18,11 +18,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.exist.xmldb.EXistResource;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
 
 @Controller
 @RequestMapping("")
 public class IndexController {
-
+  private static ConnectionProperties conn;
     private static void writeXml(Document doc,
                                  OutputStream output)
             throws TransformerException {
@@ -260,7 +267,7 @@ public class IndexController {
     }
 
     @RequestMapping("/")
-    public String index() throws ParserConfigurationException, IOException, SAXException {
+    public String index() throws Exception {
         System.out.println("Pocetna strana!");
         // xml ucitavanje
         // samo zameni komentare kodom iz main f-je
@@ -288,6 +295,230 @@ public class IndexController {
         } catch (IOException | TransformerException e) {
             e.printStackTrace();
         }
+
+        String[] args={"/db/sample/library","a1.xml","..\\a1.xml"};
+        store(conn=ConnectionProperties.loadProperties(),args);
+        String[] args2={"/db/sample/library","a1.xml"};
+        retrive(ConnectionProperties.loadProperties(),args2);
         return "index.html";
+    }
+    public static void retrive(ConnectionProperties conn, String args[]) throws Exception {
+
+
+        // initialize collection and document identifiers
+        String collectionId = null;
+        String documentId = null;
+
+        if (args.length == 2) {
+
+            System.out.println("[INFO] Passing the arguments... ");
+
+            collectionId = args[0];
+            documentId = args[1];
+        } else {
+
+            System.out.println("[INFO] Using defaults.");
+
+            collectionId = "/db/sample/library";
+            documentId = "1.xml";
+        }
+
+        System.out.println("\t- collection ID: " + collectionId);
+        System.out.println("\t- document ID: " + documentId + "\n");
+
+        // initialize database driver
+        System.out.println("[INFO] Loading driver class: " + conn.driver);
+        Class<?> cl = Class.forName(conn.driver);
+
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+
+        DatabaseManager.registerDatabase(database);
+
+        Collection col = null;
+        XMLResource res = null;
+
+        try {
+            // get the collection
+            System.out.println("[INFO] Retrieving the collection: " + collectionId);
+            col = DatabaseManager.getCollection(conn.uri + collectionId);
+            col.setProperty(OutputKeys.INDENT, "yes");
+
+            System.out.println("[INFO] Retrieving the document: " + documentId);
+            res = (XMLResource) col.getResource(documentId);
+
+            if (res == null) {
+                System.out.println("[WARNING] Document '" + documentId + "' can not be found!");
+            } else {
+
+                System.out.println("[INFO] Showing the document as XML resource: ");
+                System.out.println(res.getContent());
+
+            }
+        } finally {
+            //don't forget to clean up!
+
+            if (res != null) {
+                try {
+                    ((EXistResource) res).freeResources();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+
+            if (col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void store(ConnectionProperties conn, String args[]) throws Exception {
+
+
+
+        // initialize collection and document identifiers
+        String collectionId = null;
+        String documentId = null;
+        String filePath = null;
+
+        if (args.length == 3) {
+
+            System.out.println("[INFO] Passing the arguments... ");
+
+            collectionId = args[0];
+            documentId = args[1];
+
+            filePath = args[2];
+        } else {
+
+            System.out.println("[INFO] Using defaults.");
+
+            collectionId = "/db/sample/library";
+            documentId = "1.xml";
+
+            filePath = "data/books.xml";
+
+        }
+
+        System.out.println("\t- collection ID: " + collectionId);
+        System.out.println("\t- document ID: " + documentId);
+        System.out.println("\t- file path: " + filePath + "\n");
+
+        // initialize database driver
+        System.out.println("[INFO] Loading driver class: " + conn.driver);
+        Class<?> cl = Class.forName(conn.driver);
+
+
+        // encapsulation of the database driver functionality
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+
+        // entry point for the API which enables you to get the Collection reference
+        DatabaseManager.registerDatabase(database);
+
+        // a collection of Resources stored within an XML database
+        Collection col = null;
+        XMLResource res = null;
+
+        try {
+
+            System.out.println("[INFO] Retrieving the collection: " + collectionId);
+            col = getOrCreateCollection(collectionId);
+
+            /*
+             *  create new XMLResource with a given id
+             *  an id is assigned to the new resource if left empty (null)
+             */
+            System.out.println("[INFO] Inserting the document: " + documentId);
+            res = (XMLResource) col.createResource(documentId, XMLResource.RESOURCE_TYPE);
+
+            File f = new File(filePath);
+
+            if(!f.canRead()) {
+                System.out.println("[ERROR] Cannot read the file: " + filePath);
+                return;
+            }
+
+            res.setContent(f);
+            System.out.println("[INFO] Storing the document: " + res.getId());
+
+            col.storeResource(res);
+            System.out.println("[INFO] Done.");
+
+        } finally {
+
+            //don't forget to cleanup
+            if(res != null) {
+                try {
+                    ((EXistResource)res).freeResources();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+
+            if(col != null) {
+                try {
+                    col.close();
+                } catch (XMLDBException xe) {
+                    xe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static Collection getOrCreateCollection(String collectionUri) throws XMLDBException {
+        return getOrCreateCollection(collectionUri, 0);
+    }
+
+    private static Collection getOrCreateCollection(String collectionUri, int pathSegmentOffset) throws XMLDBException {
+        System.out.println(conn.uri+collectionUri);
+        System.out.println(conn.user);
+        System.out.println(conn.password);
+        Collection col = DatabaseManager.getCollection(conn.uri + collectionUri, conn.user, conn.password);
+
+        // create the collection if it does not exist
+        if(col == null) {
+
+            if(collectionUri.startsWith("/")) {
+                collectionUri = collectionUri.substring(1);
+            }
+
+            String pathSegments[] = collectionUri.split("/");
+
+            if(pathSegments.length > 0) {
+                StringBuilder path = new StringBuilder();
+
+                for(int i = 0; i <= pathSegmentOffset; i++) {
+                    path.append("/" + pathSegments[i]);
+                }
+
+                Collection startCol = DatabaseManager.getCollection(conn.uri + path, conn.user, conn.password);
+
+                if (startCol == null) {
+
+                    // child collection does not exist
+
+                    String parentPath = path.substring(0, path.lastIndexOf("/"));
+                    Collection parentCol = DatabaseManager.getCollection(conn.uri + parentPath, conn.user, conn.password);
+
+                    CollectionManagementService mgt = (CollectionManagementService) parentCol.getService("CollectionManagementService", "1.0");
+
+                    System.out.println("[INFO] Creating the collection: " + pathSegments[pathSegmentOffset]);
+                    col = mgt.createCollection(pathSegments[pathSegmentOffset]);
+
+                    col.close();
+                    parentCol.close();
+
+                } else {
+                    startCol.close();
+                }
+            }
+            return getOrCreateCollection(collectionUri, ++pathSegmentOffset);
+        } else {
+            return col;
+        }
     }
 }
